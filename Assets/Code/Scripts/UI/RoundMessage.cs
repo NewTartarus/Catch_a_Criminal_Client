d95590196f529720d057c386a720758424766bea
NewTartarus <new.tartarus@gmail.com>
2020-07-23 21:54:41 +0200

    CAC-0003: RoundMessage updated
    - RoundMessage outsourced to another Class

---
+++ Assets/Code/Scripts/UI/RoundMessage.cs
@@ -0,0 +1,32 @@
+﻿using ScotlandYard.Scripts.Localisation;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace ScotlandYard.Scripts.UI
+{
+    public class RoundMessage : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI roundMessageText;
+
+        public void DisplayMessage(string key, string replaceString = null)
+        {
+            string text = LocalisationSystem.GetLocalisedValue(key);
+
+            if(text.Contains("[X]") && !string.IsNullOrEmpty(replaceString))
+            {
+                text = text.Replace("[X]", replaceString);
+            }
+
+            roundMessageText.text = text;
+            this.gameObject.SetActive(true);
+        }
+
+        public void HideMessage()
+        {
+            roundMessageText.text = string.Empty;
+            this.gameObject.SetActive(false);
+        }
+    }
+}
