47cd779ab6bb55a40a561733d7a5b03d6b011162
NewTartarus <new.tartarus@gmail.com>
2021-05-29 01:50:28 +0200

    CAC-0019: Basic Template for Charakter Customization
    - Created Panel for a basic customization layout
    -- AccordionItem (collabsable items) created
    - added placeholder Icons

---
+++ Client/Assets/Code/Scripts/UI/AccordionItem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace ScotlandYard.Scripts.UI.Menu
+{
+    public class AccordionItem : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+    {
+        [SerializeField] protected GameObject icon;
+        [SerializeField] protected GameObject content;
+        [SerializeField] protected GameObject parentContentList;
+
+        protected bool collapsed = true;
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (collapsed)
+            {
+                icon.transform.Rotate(180, 0, 0);
+                content.SetActive(true);
+                
+            }
+            else
+            {
+                icon.transform.Rotate(-180, 0, 0);
+                content.SetActive(false);
+            }
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(parentContentList.GetComponent<RectTransform>());
+
+            collapsed = !collapsed;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            
+        }
+    }
+}
+
+
